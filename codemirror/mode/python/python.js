e5b023ae7d9795ad16bdc0b2bec02b9380a6cd39
Atishay25 <atishayjain2552@gmail.com>
2022-11-23 13:02:44 +0530

    Added more keywords in python

--- codemirror/mode/python/python.js
+++ codemirror/mode/python/python.js
@@ -31,7 +31,7 @@
                         "repr", "reversed", "round", "set", "setattr", "slice",
                         "sorted", "staticmethod", "str", "sum", "super", "tuple",
                         "type", "vars", "zip", "__import__", "NotImplemented",
-                        "Ellipsis", "__debug__","down()","right()","left()","up()"];
+                        "Ellipsis", "__debug__","move_down()","move_right()","move_left()","move_up()","get_direction()"];
   CodeMirror.registerHelper("hintWords", "python", commonKeywords.concat(commonBuiltins).concat(["exec", "print"]));
 
   function top(state) {
