08f60bab0c30f7ae2087d1a9a27d121636d9933c
Atishay25 <atishayjain2552@gmail.com>
2022-11-26 00:23:12 +0530

    Added Doxygen comments

--- welcome.js
+++ welcome.js
@@ -1,10 +1,20 @@
+/**
+* @file welcome.js
+* @brief JAVASCRIPT FOR Welcome Page animation.
+* @author Prolific Pythonists
+* @date 24-11-2022
+*/
+///Title of the webpage.
 var title = "Python Combat";
+///Iterator for all letters in the word "Python Combat".
 var i = 0;
+///Speed of the animation.
 var speed = 150;
 
-/// @file Sync.js
-/// @function typeWriter
-/// Module for loading and storing data
+/**
+ * Return the ratio of the inline text length of the links in an element to
+ * the inline text length of the entire element.
+ */
 function typeWriter() {
     if (i < title.length) {
         document.getElementById("heading").innerHTML += title.charAt(i);
